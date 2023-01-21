646567b889fef258ae931b8e2827205f19e1003a
Vaibhav Kumar <vaibhava.kumar@navi.com>
2023-01-21 13:47:06 +0530

    Initial Commit

---
+++ CICDtesting/CICDtestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDtestingApp.swift
+//  CICDtesting
+//
+//  Created by Vaibhav Kumar on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDtestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
